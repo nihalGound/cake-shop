18e1a0ee93136b6f39ac62dd22a0c8772a0fb7e6
nihalGound <nsnihalgound123@gmail.com>
2024-02-19 19:06:44 +0530

    added api version

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express  from "express";
 import cors from "cors";
+import { router } from "./routes/user.routes.js";
 
 const app = express();
 
@@ -13,5 +14,7 @@ app.use(cookieParser());
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"));
+app.use('/api/v1/user',router);
+
 
 export {app};
