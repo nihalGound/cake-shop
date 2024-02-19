da470515366f26157a43a20ca3f2c6cfdeb7c04b
ShivamGupta987 <118923085+ShivamGupta987@users.noreply.github.com>
2024-02-19 13:51:23 +0530

    some changes in firstname lastname like minlength etc

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,114 +1,140 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import { jwt } from "jsonwebtoken";
+import  jwt  from "jsonwebtoken";
 
-const userModel = new mongoose.Schema({
-    firstName : {
-        type:String,
-        required:[true, "User's first name required"],
+const userModel = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "User's first name required"],
+      minLength: [5, "Name should be atleast 5 Characters"],
+      maxLength: [50, "Name should be atmost 5 Characters"],
+      lowercase: true,
+      trim: true,
     },
     middleName: {
-        type:String,
+      type: String,
+      minLength: [5, "Name should be atleast 5 Characters"],
+      maxLength: [50, "Name should be atmost 5 Characters"],
+      lowercase: true,
+      trim: true,
     },
-    lastName : {
-        type:String,
-        required:[true, "User's last  name required"],
+    lastName: {
+      type: String,
+      required: [true, "User's last  name required"],
+      minLength: [5, "Name should be atleast 5 Characters"],
+      maxLength: [50, "Name should be atmost 5 Characters"],
+      lowercase: true,
+      trim: true,
     },
-    email : {
-        type:String,
-        required:[true,"email is required"],
-        unique:[true,"email already used"],
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please fill a valid email address",
+      ],
     },
-    
-    avatar : {
-        type:String,//cloudinary url
+
+    avatar: {
+      public_id: {
+        type: "string",
+      },
+      secure_url: {
+        type: "string",
+      },
     },
-    username : {
-        type:String,
-        required:[true,"username is required"],
-        unique:[true,"username already used"],
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      unique: [true, "username already used"],
     },
-    password : {
-        type:String,
-        required:[true,"password is required"]
+    password: {
+      type: String,
+      required: [true, "password is required"],
     },
-    dob : {
-        type:Date,
-        required:[true,"date of birth is required"]
+    dob: {
+      type: Date,
+      required: [true, "date of birth is required"],
     },
-    homeAdress : {
-        type:String
+    homeAdress: {
+      type: String,
     },
-    workAdress : {
-        type:String
+    workAdress: {
+      type: String,
     },
-    cart : [
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Product"
-        }
+    cart: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
     ],
-    orders : [
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"OrderPlace"
-        }
+    orders: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "OrderPlace",
+      },
     ],
-    refreshToken : {
-        type:String,
+    refreshToken: {
+      type: String,
     },
-    phone : {
-        type:String,
-        required:[true,"phone number is required"],
-        validate:{
-            validator: function(v){
-                return /^(\+91\)?[6,7,8,9]\d{9}$/.test(v);
-            },
-            message:props=>`${props.value} is not valid number`,
+    phone: {
+      type: String,
+      required: [true, "phone number is required"],
+      validate: {
+        validator: function (v) {
+          return /^(\+91\)?[6,7,8,9]\d{9}$/.test(v);
         },
+        message: (props) => `${props.value} is not valid number`,
+      },
     },
-},{timestamps:true});
+  },
+  { timestamps: true }
+);
 
-userModel.pre("save",async function(next){
-    if(!this.isModified("password"))return next();
+userModel.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-    this.password = await bcrypt.hash(this.password,10);
-    next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
-
-
-userModel.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password,this.password);
+userModel.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
 };
 
-userModel.methods.generateAccessToken = function(){
-    const token = jwt.sign(
-        {
-            username:this.username,
-            email:this.email,
-            _id:this._id,
-            firstName:this.firstName
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+userModel.methods.generateAccessToken = function () {
+  const token = jwt.sign(
+    {
+      username: this.username,
+      email: this.email,
+      _id: this._id,
+      firstName: this.firstName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+  return token;
+};
+userModel.methods.generateRefreshToken = function () {
+  const token = jwt.sign(
+    {
+      username: this.username,
+      email: this.email,
+      _id: this._id,
+      firstName: this.firstName,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+  return token;
+};
 
-    );
-    return token;
-}
-userModel.methods.generateRefreshToken = function(){
-    const token = jwt.sign(
-        {
-            username:this.username,
-            email:this.email,
-            _id:this._id,
-            firstName:this.firstName
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {expiresIn:process.env.REFRESH_TOKEN_EXPIRY}
-    );
-    return token;
-}
+export const User = mongoose.model("User", userModel);
 
 
-export const User = mongoose.model("User",userModel)
+export default User;
+
