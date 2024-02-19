f84dde325b1c71f13304f89248dd1516a41eeb38
nihalGound <nsnihalgound123@gmail.com>
2024-02-19 10:18:44 +0530

    added shop controller file

---
+++ src/controllers/shop.controller.js
@@ -0,0 +1,12 @@
+import {Shop} from "../models/Shop.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerShop = asyncHandler(async (req,res)=>{
+
+});
+
+const loginShop = asyncHandler(async (req,res)=>{
+
+});
+
+export {registerShop,loginShop};
