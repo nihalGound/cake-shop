4f1027aee5238091b983774f80cb3b6f437f8611
nihalGound <nsnihalgound123@gmail.com>
2024-02-19 19:11:45 +0530

    added register,login,logout,update-username routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import {upload} from "../middlewares/multer.middleware.js";
-import { login, register,logout,getProfile } from "../controllers/User.controller.js";
+import {auth} from "../middlewares/auth.middleware.js";
+import { loginUser, logoutUser, registerUser, updateUsername } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.post('/register',upload.single('avatar'),register);
+router.post('/register',upload.single('avatar'),registerUser);
 
-router.post('/login',login);
-router.post('/logout', logout)
-router.get('/me',isLoggedIn, getProfile)
+router.post('/login',loginUser);
+
+//secure routes
+router.post('/logout',auth,logoutUser);
+router.post('/update-username',auth,updateUsername);
+
+export {router}
