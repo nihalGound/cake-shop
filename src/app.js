c3fb3986057c5e935e07f5ddddc07156edab2c79
nihalGound <nsnihalgound123@gmail.com>
2024-02-20 08:01:03 +0530

    add multer middleware in app to receive form-data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express  from "express";
 import cors from "cors";
+import { upload } from "./middlewares/multer.middleware.js";
 import { router } from "./routes/user.routes.js";
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors({
     origin:process.env.CORS_URL,
     credentials:true
 }));
+app.use(upload.any());
 app.use(cookieParser());
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
