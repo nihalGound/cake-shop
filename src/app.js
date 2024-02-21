b25ea1560c5bd4a6604feddf81ca1c45c4722bbd
nihalGound <nsnihalgound123@gmail.com>
2024-02-21 17:33:41 +0530

    added app.user(productRouter)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import cookieParser from "cookie-parser";
 import express  from "express";
 import cors from "cors";
-import {upload} from "./middlewares/multer.middleware.js";
 import { userRouter } from "./routes/user.routes.js";
 import { shopRouter } from "./routes/shop.routes.js";
+import { productRouter } from "./routes/product.routes.js";
 
 const app = express();
 
@@ -11,13 +11,13 @@ app.use(cors({
     origin:process.env.CORS_URL,
     credentials:true
 }));
-app.use(upload.any());
 app.use(cookieParser());
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"));
 app.use('/api/v1/user',userRouter);
 app.use('/api/v1/shop',shopRouter);
+app.use('/api/v1/product',productRouter);
 
 
 export {app};
