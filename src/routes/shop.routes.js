ab8dcf80ee4991f8a56bb47bff15f9e999f50aff
nihalGound <nsnihalgound123@gmail.com>
2024-02-21 08:52:19 +0530

    added shop routes

--- src/routes/shop.routes.js
+++ src/routes/shop.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { loginShop, registerShop } from "../controllers/shop.controller";
+import { loginShop, registerShop ,logoutShop, updateUsername, updatePassword, updateName, refreshAccessToken, getUserProfile} from "../controllers/shop.controller.js";
+import {auth} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
 router.post('/shop-register',registerShop);
 
 router.post('/login-shop',loginShop);
+
+//secure routes
+router.post('/logout',auth,logoutShop);
+router.post('/update-username',auth,updateUsername);
+router.post('/update-password',auth,updatePassword);
+router.post('/update-name',auth,updateName);
+router.post('/refresh-AccessToken',refreshAccessToken);
+router.post('/get-profile',auth,getUserProfile);
+
+export const shopRouter = router;
