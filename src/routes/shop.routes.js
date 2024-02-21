6bd458129f1f4c40db04c42a45667824b2c63769
nihalGound <nsnihalgound123@gmail.com>
2024-02-21 17:37:51 +0530

    added verify-email routes

--- src/routes/shop.routes.js
+++ src/routes/shop.routes.js
@@ -1,15 +1,20 @@
 import { Router } from "express";
-import { loginShop, registerShop ,logoutShop, updateUsername, updatePassword, updateName, refreshAccessToken, getUserProfile} from "../controllers/shop.controller.js";
-import {auth} from "../middlewares/auth.middleware.js"
+import { loginShop, registerShop ,logoutShop, updateUsername, updatePassword, 
+    updateName, refreshAccessToken, getUserProfile, resetPassword, forgotPassword, 
+    sendVerificationEmail, emailVerify} from "../controllers/shop.controller.js";
+import {auth, shopAuth} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
 router.post('/shop-register',registerShop);
-
 router.post('/login-shop',loginShop);
+router.post('/reset-password',resetPassword);
+router.post('/forget-passowrd',forgotPassword);
+router.post('/verification-email',sendVerificationEmail);
+router.post('/verify-email',emailVerify);
 
 //secure routes
-router.post('/logout',auth,logoutShop);
+router.post('/logout',shopAuth,logoutShop);
 router.post('/update-username',auth,updateUsername);
 router.post('/update-password',auth,updatePassword);
 router.post('/update-name',auth,updateName);
