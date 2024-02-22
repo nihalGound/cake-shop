05f374fda6c96f8c13c2ae7b2beacaa4bd82c1ae
nihalGound <nsnihalgound123@gmail.com>
2024-02-22 13:45:52 +0530

    added allProduct routes

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { shopAuth} from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { addProduct } from "../controllers/product.controller.js";
+import { addProduct, getAllProduct, getProductByCategory, getProductBySubcategory } from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.post('/add-product',upload.single('product'),shopAuth,addProduct);
+router.get('/allProduct',getAllProduct);
+router.get('/productbycategory/:category',getProductByCategory);
+router.get('/productbycategory/:subcategory',getProductBySubcategory);
 
 export const productRouter = router; 
 
