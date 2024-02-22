3446c2f147ad328c58f9411f23515a4b250abdf9
ShivamGupta987 <118923085+ShivamGupta987@users.noreply.github.com>
2024-02-22 13:41:26 +0530

    make a update and remove route

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { shopAuth} from "../middlewares/auth.middleware.js";
+import { auth, shopAuth} from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { addProduct } from "../controllers/product.controller.js";
+import { addProduct, updateProduct } from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.post('/add-product',upload.single('product'),shopAuth,addProduct);
+
+router.route('/add-product/:id')
+    .put(auth,shopAuth,updateProduct)
+    // .delete(auth, shopAuth,removeProduct)
+
 
 export const productRouter = router; 
 
