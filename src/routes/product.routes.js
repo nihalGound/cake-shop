536c9d2e0d787224008f6715a1a8959bbcb8c059
nihalGound <nsnihalgound123@gmail.com>
2024-02-22 16:00:11 +0530

    added routes for search prodcut by price range

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import { auth, shopAuth} from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { addProduct, getAllProduct, getProductByCategory, getProductBySubcategory, removeProduct, updateProduct } from "../controllers/product.controller.js";
+import { addProduct, getAllProduct, getProductByCategory, getProductByPriceRange, getProductBySubcategory, removeProduct, updateProduct } from "../controllers/product.controller.js";
 
 const router = Router();
 
 router.post('/add-product',upload.single('product'),shopAuth,addProduct);
 router.get('/allProduct',getAllProduct);
 router.get('/productbycategory/:category',getProductByCategory);
-router.get('/productbycategory/:subcategory',getProductBySubcategory);
+router.get('/productbysubategory/:subcategory',getProductBySubcategory);
+router.get('/productbyprice/:priceRange',getProductByPriceRange);
 
 //secure routes
-router.delete('/delete-product',shopAuth,removeProduct);
+router.delete('/delete-product/:id',shopAuth,removeProduct);
 
 router.put('/update-product/:id',shopAuth,updateProduct);
 
