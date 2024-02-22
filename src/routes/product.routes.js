ed8fca59cce21aa2783a4a96182f304e6e9a19a4
ShivamGupta987 <118923085+ShivamGupta987@users.noreply.github.com>
2024-02-22 14:13:40 +0530

    put and delete

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { auth, shopAuth} from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { addProduct, updateProduct } from "../controllers/product.controller.js";
+import { addProduct, removeProduct, updateProduct } from "../controllers/product.controller.js";
 
 const router = Router();
 
