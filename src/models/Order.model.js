c4d5b9696edb4d69d3ca67f979e210bdad3b45cf
nihalGound <nsnihalgound123@gmail.com>
2024-02-23 14:51:00 +0530

    modified ordermodel

--- src/models/Order.model.js
+++ src/models/Order.model.js
@@ -1,54 +1,50 @@
 import mongoose from "mongoose";
 
-const orderStatusModel = new mongoose.Schema({
-    packaged:{
-        type:Boolean,
-        default:false,
+
+const orderItemSchema = new mongoose.Schema({
+    quantity:{
+        type:Number,
+        required:true
     },
-    shipped:{
-        type:Boolean,
-        default:false,
-    },
-    delievered:{
-        types:Boolean,
-        default:false
+    product:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Product"
     }
 });
 
 const orderModel = new mongoose.Schema({
-    cake:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Product",
+    orderItem:[{
+        type:orderItemSchema,
+        required:true,
+    }],
+    orderStatus:{
+        type:String,
+        default:"Pending",
+        required:true
     },
-    customer:{
+    shippingAddress:{
+        type:String,
+        required:true
+    },
+    totalOrderPrice:{
+        type:Number,
+        required:true
+    },
+    user:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User",
+        required:true
     },
-    quantity:{
-        type:Number,
-        default:1,
-    },
-    orderStatus:{
-        type:orderStatusModel
-    },
-    paymentDone:{
-        type:Boolean,
-        default:false,
-    },
-    orderAmount :{
-        type:Number,
+    shop:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Shop",
         required:true,
-    },    
-    delieveryContact : {
-        type:String,
-        required:true,
-        validate:{
-            validator:function(number){
-                return /^(\+91\)?[6,7,8,9]\d{9}$/.test(number);
-            },
-            message:"please enter valid number",
-        },
     },
+    orderDate:{
+        type:Date,
+        default:new Date()
+    }
+
 },{timestamps:true});
 
 export const Order = mongoose.model("Order",orderModel);
