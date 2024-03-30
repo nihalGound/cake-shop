7f820d28eb89b095247334e30135a1c0f43e7772
ShivamGupta987 <118923085+ShivamGupta987@users.noreply.github.com>
2024-03-30 10:27:02 +0530

    added a router component

--- cake-shop-client/src/App.jsx
+++ cake-shop-client/src/App.jsx
@@ -1,11 +1,30 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./main_pages/Home";
+import Login from "./Authentication/Login";
+import SignUp from "./Authentication/SignUp";
+import Phone_verification from "./Authentication/Phone_verification";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: "/login",
+    element: <Login />,
+
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+
+  },
+  {
+    path: "/verify-phone",
+    element: <Phone_verification />,
+
+  },
+
 ]);
 
 function App() {
