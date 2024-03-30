82477e437182ad4d65d9681d1838d86327387c80
ShivamGupta987 <118923085+ShivamGupta987@users.noreply.github.com>
2024-03-30 15:05:34 +0530

    firebase for authentivcation

---
+++ cake-shop-client/src/firebase/firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmvh5P0M9ZO9yaPpLHtbkzYW6fMjd1JjI",
+  authDomain: "cake-shop-61ea4.firebaseapp.com",
+  projectId: "cake-shop-61ea4",
+  storageBucket: "cake-shop-61ea4.appspot.com",
+  messagingSenderId: "32727514762",
+  appId: "1:32727514762:web:86235d5478024cf6bff773"
+};
+
+
+const app = initializeApp(firebaseConfig);
